405a480dec1b0f290e903423d939a551507570bf
Samuel <161090562+SamuelCoder1@users.noreply.github.com>
2024-11-05 19:16:59 -0500

    feat(auth): add GitHub login integration (#9)

--- config/services.php
+++ config/services.php
@@ -40,9 +40,14 @@ return [
     'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
+    'github' => [
+    'client_id' => env('GITHUB_CLIENT_ID'), 
+    'client_secret' => env('GITHUB_CLIENT_SECRET'), 
+    'redirect' => env('GITHUB_REDIRECT_URI'), 
+    ], 
+
     'discord' => [
         'webhook_url' => env('DISCORD_WEBHOOK_URL'),
     ],
-
 
 ];
