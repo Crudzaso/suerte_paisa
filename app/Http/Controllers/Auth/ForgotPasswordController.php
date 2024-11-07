29651bcf7309a5f393a5563c23fd4728e2d02ab5
SimonArias77 <sariass077@gmail.com>
2024-11-06 21:33:02 -0500

    feat(auth): create authentication views (login, register, reset password)

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
 use App\Helpers\EmailHelperGlobal;
 
 class ForgotPasswordController extends Controller
@@ -25,18 +25,14 @@ class ForgotPasswordController extends Controller
 
     public function sendResetLinkEmail(Request $request)
     {
-        // Validar que el email sea requerido y esté en el formato correcto
         $request->validate(['email' => 'required|email|exists:users,email']);
 
         try {
-            // Obtener el usuario
             $user = User::where('email', $request->email)->first();
             
-            // Crear token y enlace de restablecimiento
             $token = Password::createToken($user);
             $resetLink = route('password.reset', ['token' => $token, 'email' => $user->email]);
 
-            // Enviar el correo de restablecimiento utilizando el helper
             $this->emailHelper::sendPasswordResetEmail($user, $resetLink);
 
             return back()->with('status', 'Se ha enviado un enlace de restablecimiento a tu correo.');
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\GithubController;
+use App\Http\Controllers\AuthController;
+
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\Auth\ForgotPasswordController;
@@ -11,30 +13,6 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Rutas para autenticación con Google 
-Route::get('/auth/google', [GoogleController::class, 'login'])->name('auth.google');
-Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('auth.google.callback');
-
-//Rutas para autenticación con Github
-Route::get('auth/github', [GithubController::class, 'login'])->name('github.login');
-
-Route::get('auth/github/callback', [GithubController::class, 'callback'])->name('auth.github.callback');
-
-
-Route::prefix('auth')->group(function () {
-    Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])
-        ->name('password.request');
-
-    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
-        ->name('password.email');
-
-    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])
-        ->name('password.reset');
-
-    Route::post('/reset-password', [ResetPasswordController::class, 'reset'])
-        ->name('password.update');
 });
 
 // Rutas protegidas por autenticación
@@ -46,30 +24,34 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::post('/logout', [GoogleController::class, 'logout'])->name('logout');
     Route::get('usuarios/crear', [UserController::class, 'create'])->name('usuarios.create'); 
     Route::post('usuarios', [UserController::class, 'store'])->name('usuarios.store'); 
-    Route::get('pruebalayout', function(){
-        return view('layouts.component-layout');
-    })->name('usuarios.layouts'); 
     
-    //THIS ROUTES WERE CREATED TO CHECK THE FUNCTIONALITY OF LOGIN VIEWS  --TO DELETE   :jarenas1
-    //RESET PASSWORD
-    Route::get('new-password', function(){
-        return view('auth.auth-plantilla.new-password');
-    })->name('auth.new'); 
-
-    Route::get('reset', function(){
-        return view('auth.auth-plantilla.reset-password');
-    })->name('auth.reset'); 
-
-    Route::get('signin', function(){
-        return view('auth.auth-plantilla.sign-in');
-    })->name('auth.signin'); 
-    
-    Route::get('signup', function(){
-        return view('auth.auth-plantilla.sign-up');
-    })->name('auth.sign'); 
-
-    Route::get('twofactor', function(){
-        return view('auth.auth-plantilla.two-factor');
-    })->name('auth.twofactor'); 
+    Route::get('twofactor', function(){return view('auth.auth-plantilla.two-factor');})->name('auth.twofactor'); 
 });
+
+// Rutas para el ¿Olvidaste tu contraseña?
+Route::get('reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('auth.reset');
+
+Route::post('reset', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+Route::get('new-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+
+Route::post('new-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+
+// Rutas para autenticación con Google 
+Route::get('/auth/google', [GoogleController::class, 'login'])->name('auth.google');
+Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('auth.google.callback');
+
+//Rutas para autenticación con Github
+Route::get('auth/github', [GithubController::class, 'login'])->name('github.login');
+
+Route::get('auth/github/callback', [GithubController::class, 'callback'])->name('auth.github.callback');
+
+
+// Rutas para autenticación normal
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+// Rutas para el registro
+Route::get('registro', function() {return view('auth.register');})->name('registro');
+
+Route::post('registro', [AuthController::class, 'registro'])->name('registro.submit');
 
