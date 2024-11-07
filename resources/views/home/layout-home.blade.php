32ed120215bc5ce09130d6192e98bb37445564bf
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-06 20:39:05 -0500

    create: styles of home-layout

--- resources/views/home/layout-home.blade.php
+++ resources/views/home/layout-home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/home-layout.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -83,7 +84,7 @@
             </article>
             
 
-            
+
             <article class="social-media-footer">
                 <div class="title-media-footer">
                     <i class="bi bi-bezier2"></i>
