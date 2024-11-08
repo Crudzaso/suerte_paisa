8436cc46f584eca18021c017047045e7c5d1624d
Samuel <161090562+SamuelCoder1@users.noreply.github.com>
2024-11-08 18:37:16 -0500

    Develop (#15)
    * fix routres in deploy
    
    * Feature/notification (#4)
    
    * feat: create the function of google email
    
    * fix: repair the structure of the helpers
    
    * fix: repair errors in google
    
    * feat: creation of the discord notification (#5)
    
    * fix routres in deploy (#3)
    
    Co-authored-by: Juan José Arenas G. <juanjoarenas1218@gmail.com>
    
    * feat: add deploy-main file
    
    * fix: the secret name
    
    * feat: creacion de funcionalidad de mandar mensaje a discord
    
    * feat: arrangement and finalization of notifications
    
    ---------
    
    Co-authored-by: Juan José Arenas G. <120411525+jarenas1@users.noreply.github.com>
    Co-authored-by: SamuelCoder1 <161090562+SamuelCoder1@users.noreply.github.com>
    Co-authored-by: Juan José Arenas G. <juanjoarenas1218@gmail.com>
    Co-authored-by: antony martinez <antonymaruiz02@gmail.com>
    
    * feat:password reset creation (#6)
    
    * feat: improve login, registration and password reset notifications (#8)
    
    * feat:password reset creation
    
    * fix: update the password reset functionality
    
    * fix: fixes and improvements in the email messaging system
    
    * feat: improve login, registration and password reset notifications
    
    * feat(auth): add GitHub login integration (#9)
    
    * fix: repair a error in production with the github login (#12)
    
    * Feature/auth views (#14)
    
    * feat: update the view of login and reset-password
    
    * feat(auth): create authentication views (login, register, reset password)
    
    * fix: resolve errors in views
    
    * fix: resolve errors in views
    
    ---------
    
    Co-authored-by: SimonArias77 <sariass077@gmail.com>
    
    ---------
    
    Co-authored-by: Juan José Arenas G. <juanjoarenas1218@gmail.com>
    Co-authored-by: Emanuel Osorio <135929894+EmanuelOH@users.noreply.github.com>
    Co-authored-by: Juan José Arenas G. <120411525+jarenas1@users.noreply.github.com>
    Co-authored-by: antony martinez <antonymaruiz02@gmail.com>
    Co-authored-by: SimonArias77 <sariass077@gmail.com>

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
 use App\Helpers\EmailHelperGlobal;
 
 class ForgotPasswordController extends Controller
@@ -29,14 +30,11 @@ class ForgotPasswordController extends Controller
         $request->validate(['email' => 'required|email|exists:users,email']);
 
         try {
-            // Obtener el usuario
             $user = User::where('email', $request->email)->first();
             
-            // Crear token y enlace de restablecimiento
             $token = Password::createToken($user);
             $resetLink = route('password.reset', ['token' => $token, 'email' => $user->email]);
 
-            // Enviar el correo de restablecimiento utilizando el helper
             $this->emailHelper::sendPasswordResetEmail($user, $resetLink);
 
             return back()->with('status', 'Se ha enviado un enlace de restablecimiento a tu correo.');
