e2ec20962de47478905942d11c7ec39b20847d31
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-12 13:53:32 -0500

    create: lotteries migration

---
+++ database/migrations/2024_11_12_135212_create_lotteries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lotteries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('banner_url')->nullable();
+            $table->string('logo_url')->nullable();
+            $table->date('date_play');
+            $table->string('result')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lotteries');
+    }
+};
