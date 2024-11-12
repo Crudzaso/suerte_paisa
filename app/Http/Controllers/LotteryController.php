fa5a1f428545c8e7e63f9024fc170410e1ba28c1
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-12 16:34:43 -0500

    add: column price to lottery migration

---
+++ app/Http/Controllers/LotteryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lottery;
+use Illuminate\Http\Request;
+
+class LotteryController extends Controller
+{
+    public function index()
+    {
+        $lotteries = Lottery::all();
+        return response()->json($lotteries);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/Lottery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Lottery extends Model
+{
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'name',
+        'description',
+        'banner_url',
+        'logo_url',
+        'date_play',
+        'result',
+        'prize' 
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'lottery_user')
+                    ->withPivot('number')
+                    ->withTimestamps();
+    }
+}
