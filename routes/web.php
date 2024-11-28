a1a8fc07e3a76dbeb6ebd21e3fce385b04ccfa71
EmanuelOH <osorioemanuel0520@gmail.com>
2024-11-28 15:57:30 -0500

    fix: change in routes with repeated name

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,16 @@ use App\Http\Middleware\VerifyRoleMiddleware;
 
 // Home Route
 Route::get('/', function () {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
+    return view('welcome');
 });
 
-Route::get('', [LotteryController::class, "index"])->name("home");
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::get('detalles/{id}',[lotteryController::class, "show"])->name("details");
+Route::get('home', [LotteryController::class, 'index'])->name('home');
 
-Route::get('usuario/{id}',[UserLotteryController::class, "getLotteriesByUserId"])->name("userpurchases");
+Route::get('detalles/{id}', [LotteryController::class, 'show'])->name('details');
 
+Route::get('usuario/{id}', [UserLotteryController::class, 'getLotteriesByUserId'])->name('userpurchases');
 
 // Google Authentication Routes
 Route::prefix('auth/google')->group(function () {
@@ -39,33 +40,30 @@ Route::prefix('auth/github')->group(function () {
     Route::get('/callback', [GithubController::class, 'callback'])->name('auth.github.callback');
 });
 
-// User Routes
 Route::resource('usuarios', UserController::class);
-Route::resource('roles', RoleController::class);
-Route::resource('permisos', PermissionController::class);
 
 // Authenticated Routes (Protected by Auth Middleware)
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::post('logout', [GoogleController::class, 'logout'])->name('logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
     Route::get('twofactor', function () { return view('auth.auth-plantilla.two-factor'); })->name('auth.twofactor'); 
+
     Route::post('/usuarios/lotteries/assign-number', [UserLotteryController::class, 'buyLottery'])->name('assign.number');
     
     // Admin Routes (Protected by Role Middleware)
     Route::middleware(['role:admin'])->group(function () {
         Route::get('usuarios/eliminados', [UserController::class, 'trashed'])->name('usuarios.trashed');
         Route::post('usuarios/{id}/restaurar', [UserController::class, 'restore'])->name('usuarios.restore');
-        
+        Route::resource('roles', RoleController::class);
+        Route::resource('permisos', PermissionController::class);
     });
 });
 
-
 // Password Reset Routes
 Route::get('reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('auth.reset');
-Route::post('reset', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::post('reset-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('new-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('new-password', [ResetPasswordController::class, 'reset'])->name('password.update');
-
 
 // Registration Routes
 Route::get('registro', function () { return view('auth.register'); })->name('registro');
